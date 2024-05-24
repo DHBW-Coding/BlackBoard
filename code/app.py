814d325fb16a6354ad2703d7e8f04d1957c781ed
Christophgru <christoph@familie-gruender.de>
2024-05-24 18:11:01 +0200

    cntl-c handling

--- code/app.py
+++ code/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from config import Config
 from views import blackboard_bp
 from logger import logger  # Import the logger to initialize it early
+import signal
+import sys
 
 def create_app():
     app = Flask(__name__)
@@ -9,6 +11,17 @@ def create_app():
     app.register_blueprint(blackboard_bp, url_prefix='/v1')
     return app
 
+def signal_handler(sig, frame):
+    print('Ctrl+C detected! Shutting down gracefully...')
+    logger.info('Ctrl+C detected! Shutting down gracefully...')
+    sys.exit(0)
+
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=5000)
+    signal.signal(signal.SIGINT, signal_handler)
+    try:
+        app.run(host='0.0.0.0', port=5000)
+    except KeyboardInterrupt:
+        print('Server interrupted by user')
+        logger.info('Server interrupted by Server Commandline')
+        sys.exit(0)
